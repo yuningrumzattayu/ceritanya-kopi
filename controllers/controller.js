766efd38f316a4088337a7e2109c076536ff743e
yuningrumzattayu <ningrumzty@gmail.com>
2026-06-25 15:33:14 +0700

    login register

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,30 +6,87 @@ const {
   Order,
   OrderDetail,
 } = require("../models");
+const bcrypt = require("bcryptjs");
+const { formatRupiah } = require("../helpers/helper");
 
 class Controller {
   // login register
-  static landingpage(req, res) {
-    res.render("landing");
-  }
-
-  static registerForm(req, res) {
-    res.render("register");
-  }
-
-  static async postRegister(req, res) {
+  static async landingpage(req, res) {
     try {
+      res.render("landing");
     } catch (error) {
       res.send(error);
     }
   }
 
+  static async registerForm(req, res) {
+    try {
+      res.render("register", {
+        errors: null,
+      });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+  static async postRegister(req, res) {
+    try {
+      const { name, email, password } = req.body;
+      const user = await User.create({
+        name,
+        email,
+        password,
+        role: "Customer",
+      });
+
+      await MembershipCard.create({
+        UserId: user.id,
+        cardNumber: "CC-" + String(user.id).padStart(4, "0"),
+        level: "Bronze",
+        points: 0,
+      });
+
+      res.redirect("/login");
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        const errors = error.errors.map((el) => el.message);
+        res.render("register", {
+          errors,
+        });
+      } else {
+        res.send(error);
+      }
+    }
+  }
+
   static loginForm(req, res) {
-    res.render("login");
+    res.render("login", {
+      errors: null,
+    });
   }
 
   static async postLogin(req, res) {
     try {
+      const { email, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        return res.render("login", {
+          errors: ["Invalid Email / Password"],
+        });
+      }
+      const isValid = bcrypt.compareSync(password, user.password);
+
+      if (!isValid) {
+        return res.render("login", {
+          errors: ["Invalid Email / Password"],
+        });
+      }
+
+      res.redirect("/menus");
     } catch (error) {
       res.send(error);
     }
@@ -40,6 +97,16 @@ class Controller {
   // Menu
   static async menuList(req, res) {
     try {
+      let menus = await Menu.findAll({
+        include: Category,
+      });
+
+      res.render("menus", {
+        menus,
+        formatRupiah,
+      });
+
+      res.render("menus");
     } catch (error) {
       res.send(error);
     }
